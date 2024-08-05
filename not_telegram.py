5fb47ff6f47ecbc2113056675c4e7c487a0fae3a
DavidovAA <aleksei-davydov8@mail.ru>
2024-08-05 15:06:51 +0900

    Add files via upload

---
+++ not_telegram.py
@@ -0,0 +1,59 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+
+cursor.execute(' CREATE INDEX IF NOT EXISTS idx_email ON Users (email)')
+
+# for i in range(1, 11):
+#    cursor.execute(' INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
+#                   (f'User{i}', f'ex{i}@gmail.com', f'{i*10}', '1000'))
+
+# for i in range(1, 11, 2):
+#    cursor.execute(' UPDATE Users SET balance = ? WHERE id = ?', (500, i))
+
+# for i in range(1, 11, 3):
+#    cursor.execute(' DELETE FROM Users WHERE id = ?', (i,))
+
+# cursor.execute(' SELECT username, email, age, balance FROM Users WHERE age != 60')
+# users = cursor.fetchall()
+# for user in users:
+#     print(f'Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}')
+
+cursor.execute(' DELETE FROM Users WHERE id = ?', (6,))
+
+cursor.execute(' SELECT COUNT(*) FROM Users')
+sum_users = cursor.fetchone()[0]
+print(sum_users)
+
+cursor.execute(' SELECT SUM(balance) FROM Users')
+sum_balance = cursor.fetchone()[0]
+print(sum_balance)
+
+cursor.execute('SELECT AVG(balance) FROM Users')
+avg_balance = cursor.fetchone()[0]
+print(avg_balance)
+
+# cursor.execute('SELECT SUM(age) FROM Users')
+# total1 = cursor.fetchone()[0]
+# print(total1)
+# cursor.execute('SELECT COUNT(*) FROM Users')
+# total2 = cursor.fetchone()[0]
+# print(total1, total1/total2)
+# cursor.execute('SELECT AVG(age) FROM Users')
+# avg_age = cursor.fetchone()[0]
+# print(avg_age)
+
+
+connection.commit()
+connection.close()
